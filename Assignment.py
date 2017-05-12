444cf357769c398d8ce13fc0d92199424283fc81
Walaga <walagapriscilla@gmail.com>
2017-05-12 04:16:08 -0700

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -3,8 +3,9 @@ import calendar  #importing inbuilt calendar module
 a=input("What is your year of birth?\n")
 b=input("Enter your month of birth:\nFor January enter 1\n    February    2\n    March       3\n    April       4\n    May         5\n    June        6\n    July        7\n    August      8\n    September   9\n    October    10\n    November   11\n    December   12\n")
 c=input("Enter the date on which you were born(1-30)\n")
-x=calendar.weekday(year=int(a), month=int(b), day=int(c))
+x=calendar.weekday(year=int(a), month=int(b), day=int(c)) #module gives the index of the day from the list ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
+#Applying a conditional function to assign each date a corresponding prefix 
 if int(c) == 1:
     d = c +"st"
 elif int(c)==2:
@@ -25,6 +26,9 @@ else:
     d = c+"th"
 #apart from the elif function we could just have used a list.
 #the example this: prefix=  ['st','nd','rd']+ 17*['th']+['st','nd','rd']+17*['th']+['st']
+
+
+#Applying a conditional function to assign a variable name for the month
 if int(b) == 1:
        m = "January"
 elif int(b) == 2:
@@ -50,6 +54,9 @@ elif int(b) == 11:
 else:
     m = "December"
 
+    
+    
+    #Applying a conditional function to assign the result from the module used to the respective day
 if x == 0:
     y = "Monday"
 elif x == 1:
